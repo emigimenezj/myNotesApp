f690862fb482c8119df4361f15dc0853c0977cb5
Emilio Giménez <emigimenezj@zohomail.com>
2021-06-08 16:39:26 -0300

    Provides more verbose description of command's functionality in the 'help' area.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const chalk = require("chalk");
 const yargs  = require("yargs");
 const notes = require("./notes");
 
@@ -7,20 +8,23 @@ yargs.version("0.1.0");
 // Create add command
 yargs.command({
     command: "add",
-    describe: "Add a new note",
+    describe: "--------------------------------------------------------------\n" +
+        "Allows you to add a new note.\n" +
+        `It ${chalk.greenBright.underline("requires")} a title and body for the note.\n` +
+        "Set the flag '-i' to add a note to the important list.",
     builder: {
         title: {
-            describe: "Note title",
+            describe: `Use ${chalk.redBright("--title=\"TITLE\"")} to provide a title.`,
             demandOption: true,
             type: 'string'
         },
         body: {
-            describe: "Note body",
+            describe: `Use ${chalk.redBright("--body=\"BODY\"")} to provide a body.`,
             demandOption: true,
             type: 'string'
         },
         i: {
-            describe: "Note important tier",
+            describe: "Set this flag, to add a note to the important list.",
             type: 'boolean'
         }
     },
@@ -32,7 +36,10 @@ yargs.command({
 // Create remove command
 yargs.command({
     command: "remove",
-    describe: "Remove a note",
+    describe: "--------------------------------------------------------------\n" +
+        "Provide the number of the note you want to remove.\n" +
+        "You can provide a set of numbers to delete multiples notes simultaneously.\n" +
+        "Set the flag '-i' to remove your important notes.\n",
     handler(argv) {
         notes.removeNote(argv._.slice(1), argv.i ||false);
     }
@@ -41,7 +48,9 @@ yargs.command({
 // Create list command
 yargs.command({
     command: "list",
-    describe: "List your notes",
+    aliases: "l",
+    describe: "--------------------------------------------------------------\n" +
+        "List just the title of your notes.\n",
     handler() {
         notes.listNotes();
     }
@@ -51,7 +60,9 @@ yargs.command({
 // Create listall command
 yargs.command({
     command: "listall",
-    describe: "List all notes with their bodies",
+    aliases: "la",
+    describe: "--------------------------------------------------------------\n" +
+        "List all notes including their titles and bodies.\n",
     handler() {
         notes.listAllNotes();
     }
@@ -61,7 +72,10 @@ yargs.command({
 // Create read command
 yargs.command({
     command: "read",
-    describe: "Read a note",
+    describe: "--------------------------------------------------------------\n" +
+        "Provide the number of the note you want to read.\n" +
+        "You can provide a set of numbers to read multiples notes simultaneously.\n" +
+        "Set the flag '-i' to read your important notes.\n",
     handler(argv) {
         notes.readNote(argv._.slice(1), argv.i || false);
     }
@@ -69,10 +83,14 @@ yargs.command({
 
 yargs.command({
     command: "swap",
-    describe: "Swap positions between two notes",
+    describe: "--------------------------------------------------------------\n" +
+        "Provide two numbers (if there are more, ignore the rest) of notes to swap position on the list.\n" +
+        "Set the flag '-i' to swap your important notes.\n",
     handler(argv) {
         notes.swapNotes(argv._[1], argv._[2], argv.i || false);
     }
 });
 
 yargs.parse();
+
+// commit -m "Provides more verbose description of command's functionality in the 'help' area."
